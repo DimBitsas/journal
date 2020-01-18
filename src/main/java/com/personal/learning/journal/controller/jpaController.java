ed797e4a2ab9c208ccb10b464aab1030ab968afb
Dimitris Bitsas <dimitrismplink72@gmail.com>
2020-01-18 20:16:12 +0200

    Jpa controller improvements: retrieve entry, create entry, delete entry

--- src/main/java/com/personal/learning/journal/controller/jpaController.java
+++ src/main/java/com/personal/learning/journal/controller/jpaController.java
@@ -3,10 +3,14 @@ package com.personal.learning.journal.controller;
 import com.personal.learning.journal.model.Entry;
 import com.personal.learning.journal.service.EntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class jpaController {
@@ -18,4 +22,31 @@ public class jpaController {
 
     @GetMapping("jpa/entries")
     public List<Entry> retrieveAllEntries(){return service.findAll();}
+
+    @GetMapping("jpa/entry/{id}")
+    public Entry retrieveEntry(@PathVariable Long id){
+        Optional<Entry> entry = service.findById(id);
+
+        if(!entry.isPresent()){
+            //throw new EntryNotFoundException("id-"+id);
+        }
+
+        return entry.get();
+    }
+
+    @PostMapping("jpa/entries")
+    public void createEntry(@Valid @RequestBody Entry entry){
+        Entry savedEntry = service.save(entry);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id")
+                .buildAndExpand(savedEntry.getId())
+                .toUri();
+
+        ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("jpa/entries/{id}")
+    public void deleteEntry(@PathVariable Long id){service.deleteById(id);}
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into entry values(0, 'java beans', '12345');
-insert into entry values(1, 'java kati', '12345');
+insert into entry values(0, 'einai auto pou blablabla', 'java beans');
+insert into entry values(1, 'einai blablablabla', 'servlets');
 
